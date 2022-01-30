258bfdd044502b5183fcced0119a3a4df711463f
numanair <numanair@gmail.com>
2022-01-29 16:48:22 -0800

    multimap working

--- src/xiao_control_surface.cpp
+++ src/xiao_control_surface.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <Control_Surface.h>
 
+const int NUM_SLIDERS = 5;
+
 // MIDI Interface for use with Hairless
 //HairlessMIDI_Interface midi;
 USBMIDI_Interface midi;
@@ -15,10 +17,20 @@ CCPotentiometer volumePotentiometers[] = {
   {A4, {MIDI_CC::Channel_Volume, CHANNEL_5}},
 };
 
-int inputval[] =  {2, 32,    145,   353,    909,  4064};
-int outputval[] = {0, 85, 171, 256, 341,  1023};
+// make sure Control Surface is using 12-bit ADC 
+constexpr uint8_t ADC_BITS = 12;
 
-// note: the _in array should have increasing values
+int inputval[] =  {20, 16383};
+int outputval[] = {0,  16383};
+// Note: 16383 = 2¹⁴ - 1 (the maximum value that can be represented by
+// a 14-bit unsigned number
+const int arrayQty = 2;
+// number of elements in the MultiMap arrays
+
+Timer<millis> timer = 500; // milliseconds between sending serial data
+
+// note: the _in array must have increasing values
+// same function as MultiMap Arduino library
 int multiMap(int val, int* _in, int* _out, uint8_t size)
 {
   // take care the value is within range
@@ -37,30 +49,43 @@ int multiMap(int val, int* _in, int* _out, uint8_t size)
   return (val - _in[pos-1]) * (_out[pos] - _out[pos-1]) / (_in[pos] - _in[pos-1]) + _out[pos-1];
 }
 
-// The filtered value read when potentiometer is at the 0% position
-constexpr analog_t minimumValue = 255;
-// The filtered value read when potentiometer is at the 100% position
-constexpr analog_t maximumValue = 16383 - 255;
-
 analog_t mappingFunction(analog_t raw) {
-    // make sure that the analog value is between the minimum and maximum
-    // raw = constrain(raw, minimumValue, maximumValue);
-    // map the value from [minimumValue, maximumValue] to [0, 16383]
-    int mapped = multiMap(raw, inputval, outputval, 6);
-    return map(mapped, 2, 1023, 0, 16383);
-    // Note: 16383 = 2¹⁴ - 1 (the maximum value that can be represented by
-    // a 14-bit unsigned number
+    int mapped = multiMap(raw, inputval, outputval, arrayQty);
+    // adjusts slider output for better linearity
+    return mapped;
 }
+
+void sendSliderValues();
 
 // Initialize the Control Surface
 void setup() {
   for (auto &volumePotentiometers : volumePotentiometers)
   volumePotentiometers.map(mappingFunction);
   Control_Surface.begin();
-  // Serial.begin(115200);
+  Serial.begin(115200);
 }
 
 // Update the Control Surface
 void loop() {
   Control_Surface.loop();
+  for (size_t i = 0; i < 5; i++)
+  {
+  // Serial.println(volumePotentiometers[1].getRawValue());
+    if (timer)
+    {
+    sendSliderValues();
+    }
+  }
+}
+
+void sendSliderValues() {
+  String builtString = String("");
+
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    builtString += String((int)volumePotentiometers[i].getRawValue());
+    if (i < NUM_SLIDERS - 1) {
+      builtString += String("|");
+    }
+  }
+  Serial.println(builtString);
 }
